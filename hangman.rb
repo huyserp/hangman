6e02a0fa8f052a902c1678e86d201f8a73790354
Peter Huyser <huyserp@gmail.com>
2020-06-28 11:22:24 +0200

    downloaded list, filtered for 6 <= word => 12

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,24 @@
+require 'pry'
+#################### DICTIONARY PREVIOUSLY FILTERED #####################
+# only contains words at least 6 letter and no more than 12 letter long #
+
+# dictionary = File.readlines "5desk.txt"
+# dictionary = dictionary.map { |word| word.strip }.select { |word| word.size <= 12 && word.size >= 6 }
+# filename = "5desk_FILTERED.txt"
+# File.open(filename, 'w') do |file|
+#     file.puts dictionary
+# end
+
 class Hangman
     attr_accessor :player, :wins
 
     def initialize(player)
         @player = player
         @wins = wins
-        @dictionary = ""
+        @dictionary = File.readlines "5desk_FILTERED.txt"
         @word_to_guess = ""
-        
+
+        set_word_to_guess
     end
 
 
