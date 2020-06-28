c8e852fb9e5af2b857fa5fa32d559888ab1f3ef5
Peter Huyser <huyserp@gmail.com>
2020-06-28 09:47:39 +0200

    set up program and README.md

---
+++ hangman.rb
@@ -0,0 +1,13 @@
+class Hangman
+    attr_accessor :player, :wins
+
+    def initialize(player)
+        @player = player
+        @wins = wins
+        @dictionary = ""
+        @word_to_guess = ""
+        
+    end
+
+
+end
